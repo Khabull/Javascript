54513e535dbf6927f060aab553f131bd6c35f569
Khabull <a.khadhuu@gmail.com>
2024-07-22 19:59:39 +0800

    "

--- Hicheel2/3.js
+++ Hicheel2/3.js
@@ -1,6 +1,9 @@
-let a=prompt("cola")
-let b=prompt("water")
+    let a = prompt("A утга оруулна уу")
+    let b = prompt("Б утга оруулна уу")
+console.log("А утга бол " + a + " Б утга бол " +b )
+
 let c=a
 a=b
 b=c
-console.log(a,b)
+// console.log(a,b)
+console.log("солигдсон утга A " +a+ " солигдсон утга Б " +b)
