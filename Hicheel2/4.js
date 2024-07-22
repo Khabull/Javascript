28d77ecbfa131611b2d6c6dab67e25994da06ae4
Khabull <a.khadhuu@gmail.com>
2024-07-22 20:25:14 +0800

    bodlogo

--- Hicheel2/4.js
+++ Hicheel2/4.js
@@ -1,6 +1,6 @@
 let B=prompt("тоо1")
 let C=prompt("тоо2")
 let D=prompt("тоо3")
-console.log("A="B*C-D " илэрхийллийн хариу")
+console.log("A=B*C-D")
 A=B*C-D
-console.log(" илэрхийллийн хариу" A);
+console.log("илэрхийллийн хариу " +A);
