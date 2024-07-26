29050a425b7c4d6a731ce56f981ce8950e6d129a
Khabull <a.khadhuu@gmail.com>
2024-07-26 19:58:34 +0800

    hurd

--- Hicheel3/ex6.js
+++ Hicheel3/ex6.js
@@ -1,6 +1,24 @@
+// let speed = prompt("тоо оруул");
+// if (speed >= 80 && speed < 85) {
+//   console.log("1 оноо хасагдсан, эрх хасагдаагүй");
+// } else if (speed >= 85 && speed < 90) {
+//   console.log("2 оноо хасагдсан, эрх хасагдаагүй");
+// } else if (speed >= 90 && speed < 95) {
+//   console.log("3 оноо хасагдсан, эрх хасагдаагүй");
+// } else if (speed >= 95 && speed < 100) {
+//   console.log("4 оноо хасагдсан, эрх хасагдаагүй");
+// } else if (speed >= 100 && speed < 105) {
+//   console.log("5 оноо хасагдсан, эрх хасагдсан");
+// }
 let speed = prompt("тоо оруул");
-let onoo = 1;
-if (speed >= 80 && speed < 85) {
-  console.log("хурд хэтэрсэн " + onoo + " оноо хасагдсан");
+
+if (speed < 80) {
+  console.log("хурд хэтрээгүй ");
 } else {
+  let onoo = parseInt((speed - 80) / 5);
+  if (onoo >= 5) {
+    console.log(onoo + " оноо хасагдсан, эрх хасагдсан");
+  } else if (onoo < 5) {
+    console.log(onoo + " оноо хасагдсан, эрх хасагдаагүй");
+  }
 }
