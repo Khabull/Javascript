341a6c46a383b1d8d3a2d91e3685f162f94ef9f3
Khabull <a.khadhuu@gmail.com>
2024-07-29 18:50:35 +0800

    for loop

---
+++ hicheel4/ex1.js
@@ -0,0 +1,4 @@
+let n = prompt("тоо оруулна уу");
+for (i = 1; i <= n; i++) {
+  console.log("Codely№ " + i);
+}
---
+++ hicheel4/ex2.js
@@ -0,0 +1,12 @@
+let n = prompt("тоо оруулна уу");
+for (i = 1; i <= n; n--) {
+  console.log(n);
+}
+
+// for (i = n; i >= 0; n--) {
+//     console.log(n);
+//   }
+
+//   for (i = 0; i <= n; i++) {
+//     console.log(n-i);
+//   }
