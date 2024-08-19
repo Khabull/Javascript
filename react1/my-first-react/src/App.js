189027c7cf68c975f68ac5b7cead3c471f8c718c
Khabull <a.khadhuu@gmail.com>
2024-08-19 19:34:52 +0800

    box

--- react1/my-first-react/src/App.js
+++ react1/my-first-react/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import MyFirstComp from "./components/MyFirstComp";
+import Massive from "./components/Massive";
 import Box from "./components/Box";
 
 function App() {
   return (
     <div>
+      <Massive />
       <Box text="BOX 1" coloR="1" />
       <Box text="BOX 2" coloR="2" />
       <Box text="BOX 3" coloR="3" />
---
+++ react1/my-first-react/src/components/Massive.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Massive = () => {
+  return <div></div>;
+};
+
+export default Massive;
